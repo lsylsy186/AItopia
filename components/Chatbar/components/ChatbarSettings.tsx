2a6456efe0b6f87c84c43bb10d2478c7f4132190
lsylsy186 <tommy450845821@gmail.com>
2023-09-08 19:53:56 +0800

    feat: 增加联系管理员页面

--- components/Chatbar/components/ChatbarSettings.tsx
+++ components/Chatbar/components/ChatbarSettings.tsx
@@ -1,4 +1,5 @@
 import { IconFileExport, IconSettings } from '@tabler/icons-react';
+import { getMeta, ENVS } from '@/constants';
 import { useContext, useState } from 'react';
 
 import { useTranslation } from 'next-i18next';
@@ -48,12 +49,22 @@ export const ChatbarSettings = () => {
     </>
   }
 
+  const meta = getMeta(window.location.href || '');
+  const { env } = meta;
+
+  // 支持联系方式
+  const ableContact = env === ENVS.normal || ENVS.local;
+
   return (
     <div className="flex flex-col items-center space-y-1 border-t border-white/20 pt-1 text-sm">
       <SidebarButton
         text={BalanceComp()}
         icon={<IconAlien size={18} />}
-        onClick={() => { router.push('/other/contact') }}
+        onClick={() => {
+          if (!ableContact) return;
+          router.push('/other/contact')
+        }
+        }
       />
       {conversations.length > 0 ? (
         <ClearConversations onClearConversations={handleClearConversations} />

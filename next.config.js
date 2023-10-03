d991e65a59fcc5da7b3b170865dba49accb6bbce
lsylsy186 <tommy450845821@gmail.com>
2023-10-03 14:39:55 +0800

    feat: 上传图片能力初版

--- next.config.js
+++ next.config.js
@@ -3,10 +3,26 @@ const { i18n } = require('./next-i18next.config');
 //   openAnalyzer: process.env.ANALYZE === 'true',
 // });
 
+const STORE_ID = process.env.BLOB_READ_WRITE_TOKEN?.match(
+  /^vercel_blob_rw_([a-z0-9]+)_[a-z0-9]+$/i,
+)?.[1].toLowerCase();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
   reactStrictMode: true,
+  images: {
+    imageSizes: [200, 400, 1050],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: `${STORE_ID}.public.blob.vercel-storage.com`,
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    minimumCacheTTL: 31536000,
+  },
   // TODO：edge环境需要移除这个包
   // experimental: {
   //   serverComponentsExternalPackages: ['bcrypt'],

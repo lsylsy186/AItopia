99efbddb7ec780c84f418d494928d2e1b28bb92b
lsylsy186 <tommy450845821@gmail.com>
2023-10-10 13:14:05 +0800

    fix: 增加小红书

--- constants/role/furniture/furnxiaohongshu.ts
+++ constants/role/furniture/furnxiaohongshu.ts
@@ -4,7 +4,7 @@ import { formType, IRole } from '../type';
 export const furnxiaohongshu: IRole = {
   index: 52,
   img: '/images/commonxiaohongshu.jpg',
-  imgAlt: 'restxiaohongshu',
+  imgAlt: 'furnxiaohongshu',
   title: '软文助手',
   description: '基于介绍的产品和品类撰写软文',
   example: '作为一名资深的{}行业从业者（{}）, 想在小红书平台做一名{}（品类）介绍博主，这次主要推荐的是{}产品',

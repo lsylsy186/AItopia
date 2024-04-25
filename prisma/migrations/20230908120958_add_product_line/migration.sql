115762221101b876f6a4a9e124e3bba070972f57
lsylsy186 <tommy450845821@gmail.com>
2024-04-25 11:50:48 +0800

    feat: 修改数据库

--- prisma/migrations/20230908120958_add_product_line/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateEnum
-CREATE TYPE "Role" AS ENUM ('Admin', 'Mod', 'Sell', 'User');
-
--- CreateEnum
-CREATE TYPE "ProductLine" AS ENUM ('Normal', 'Hebao');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "productLine" "ProductLine" NOT NULL DEFAULT 'Normal',
-ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'User';
--- prisma/migrations/20231016115914_add_local_product_line/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterEnum
-ALTER TYPE "ProductLine" ADD VALUE 'Local';
